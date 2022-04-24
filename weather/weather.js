9c3906d9e4c0f77d4ba31efc0cf28414008ac337
Bfitzp6 <97933181+Bfitzp6@users.noreply.github.com>
2022-04-24 13:07:56 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,9 +1,7 @@
 // Create a new map centered on the continental US
 var map = L.map('map').setView([38, -95], 4);
-
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl).addTo(map);
-
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
@@ -11,7 +9,6 @@ var radarDisplayOptions = {
   transparent: true
 };
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
-
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
   L.geoJSON(data, {
